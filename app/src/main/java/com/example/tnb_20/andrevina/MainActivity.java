3b8b75b5f4944fb8518745a0c0d94cc279ab601b
Roger <rogertim3@gmail.com>
2018-11-11 16:44:01 +0100

    Segundo Commit insertando metodos en el main y cambiando un poco el XML

--- app/src/main/java/com/example/tnb_20/andrevina/MainActivity.java
+++ app/src/main/java/com/example/tnb_20/andrevina/MainActivity.java
@@ -13,40 +13,60 @@ import com.example.tnb_20.andrevina.R;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private int rango;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Random random = new Random();
-        final int rango = random.nextInt(100);
+        startApp();
+
+
+    }
+
+    private void startApp() {
+
         final Button button = findViewById(R.id.button);
-        final EditText editText = findViewById(R.id.editText);
+
+        rango = generateRandom();
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String st = String.valueOf(editText.getText());
-                int numero = Integer.parseInt(st);
-                if(numero > rango){
-                    Context context = getApplicationContext();
-                    CharSequence text = "Pon un numero mas pequeño";
-                    int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                }else if(numero < rango){
-                    Context context = getApplicationContext();
-                    CharSequence text = "Pon un numero mas grande";
-                    int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                }else if(numero == rango){
-                    Context context = getApplicationContext();
-                    CharSequence text = "Lo has adivinado";
-                    int duration = Toast.LENGTH_SHORT;
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                }
+                adivinarNumero();
             }
         });
+    }
+
+    private int generateRandom() {
+
+        int numeroAleatorio = (int) (Math.random() * 100 + 1);
+
+        return numeroAleatorio;
+    }
+
+    public void adivinarNumero() {
+        final EditText editText = findViewById(R.id.editText);
+        String st = String.valueOf(editText.getText());
+        int numero = Integer.parseInt(st);
+        if (numero > rango) {
+            Context context = getApplicationContext();
+            CharSequence text = "Pon un numero mas pequeño";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        } else if (numero < rango) {
+            Context context = getApplicationContext();
+            CharSequence text = "Pon un numero mas grande";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        } else if (numero == rango) {
+            Context context = getApplicationContext();
+            CharSequence text = "Lo has adivinado";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            rango = generateRandom();
+        }
     }
 
 }
